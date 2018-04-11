2d01aa7f1cf8434bc0cb36cb141b5160caf618aa
StonyMoon <zjmeow@gmail.com>
2018-04-11 21:00:12 +0800

    create global application

---
+++ app/src/main/java/com/stonymoon/bboard/app/MyApplication.java
@@ -0,0 +1,11 @@
+package com.stonymoon.bboard.app;
+
+import android.app.Application;
+
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
